eb735bf62df2d66df0b4293c2dbade6ef1efd701
zed.shawn <alizeeshan1999@gmail.com>
2021-09-19 13:10:31 +0530

    added update password & delete user

--- server/src/Schema/Mutations/User.ts
+++ server/src/Schema/Mutations/User.ts
@@ -1,6 +1,7 @@
-import { GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString } from "graphql";
 import { UserType } from "../TypeDefs/User";
 import { Users } from "../../Entities/Users";
+import { resolveModuleName } from "typescript";
 
 export const CREATE_USER = {
   type: UserType,
@@ -13,5 +14,36 @@ export const CREATE_USER = {
     const { name, username, password } = args;
     await Users.insert({ name, username, password });
     return args;
+  },
+};
+export const UPDATE_PASSWORD = {
+  type: UserType,
+  args: {
+    username: { type: GraphQLString },
+    oldPassword: { type: GraphQLString },
+    newPassword: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const userData = await Users.findOne({ username: username });
+    if (userData?.password == oldPassword) {      
+      return await Users.update(
+        { username: username },
+        { password: newPassword }
+      );
+    } else {
+      throw new Error("Wrong Password!");
+    }
+  },
+};
+
+export const DELETE_USER = {
+  type: UserType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    const { id } = args;
+    await Users.delete(id);
   },
 };
